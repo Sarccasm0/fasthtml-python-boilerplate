c573f27a56c2e4c96ae08d64049bb93883f6b4c9
Jefferson <jeffers1917@hotmail.com>
2024-08-08 00:28:04 -0500

    Rollback

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from fasthtml.fastapp import *
 
-def boton(boton_texto, boton_link):
-    return A(boton_texto, href=boton_link, _class="button")
-
-posts = [
-    {'imageUrl': '/img1.png', 'link': '/img1.png'},
-    {'imageUrl': '/img2.png', 'link': '/img2.png'},
-    # Add more posts as needed
-]
-
-def texto(title, statement, boton_texto, boton_link):
-    return Div(
-        H1(title),
-        P(statement),
-        boton(boton_texto, boton_link),
-        cls="container-fluid"
-    )
-
 app, rt = fast_app(
     hdrs=(
         Title("My Custom Tab Title"),  # Set the title here
@@ -26,16 +9,33 @@ app, rt = fast_app(
     )
 )
 
-# Routes
+
+def boton(boton_texto, boton_link):
+    return A(boton_texto,href=boton_link,_class="button")
+
+posts = [
+     {'imageUrl': '/img1.png', 'link': '/img1.png'},
+    {'imageUrl': '/img2.png', 'link': '/img2.png'},
+   
+]
+
+
+
+def texto(title, statement,boton_texto,boton_link):
+    return Div(H1(title),P(statement),boton(boton_texto,boton_link),cls="container-fluid")
+
+
+#Routes
+
+
 @rt("/")
 def get():
-    return Div(texto("Hi, my name is Jefferson 😎", "Multimedia Designer.. Kinda, 3D noob and aspiring programmer.", "Know what things I do when I get bored 🐱‍👤", "/about"))
+    return Div(texto("Hi, my name is Jefferson 😎",P("Multimedia Designer.. Kinda, 3D noob and aspiring programmer."),"Know what things I do when I get bored 🐱‍👤","/about"))
 
 @rt("/about")
 def get():
-    return Div(
-        texto("INSERT PORTFOLIO 🐱‍🚀", "A 'Place' to show what I'm interested in", "Back to The Casa", "/"),
-        Div(
+    return Div(texto("INSERT PORTFOLIO 🐱‍🚀","A  'Place' to show what I'm interested in ","Back to The Casa","/"),
+               Div(
             *[Div(
                 A(
                     Img(src=post['imageUrl']),
@@ -45,14 +45,11 @@ def get():
                 cls='grid-item'
             ) for post in posts],
             cls='grid-container'
-        ),
-        cls='container-fluid'
+        )
     )
 
-@rt("/egg")
-def get():
-    return Div(
-        texto("Nuevo componente", "Pasando componentes XD", "Volver a la pagina principal", "/")
-    )
+
+
+
 
 serve()
